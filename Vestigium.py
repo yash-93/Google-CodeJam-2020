2c1dc36c320ebb51ad92ba6c95256bfec1663d7a
yash-93 <yashdeepbachhas93@gmail.com>
2020-04-04 20:41:08 +0530

    update

--- Vestigium.py
+++ Vestigium.py
@@ -1,16 +1,37 @@
 import numpy as np
+K = []
+R = []
+C = []
 
 T = int(input())
-
-for case in range(T):
-	Mat = []
-	size = int(input())
-
-	for _ in range(size):
-		row = list(map(int,input().split()))[:size] 
-		Mat.append(row)
-
-	diag_ele = np.array(Mat).diagonal()
-	k = sum(diag_ele)
-
-	print(f"Case #{case}: {k} {case} {case}")
+if T >= 1 and T <= 100:
+	for case in range(T):
+		Mat = []
+		N = int(input())
+		r = 0
+		c = 0
+		k = 0
+		break_flag = False
+		if N >= 2 and N <= 100:
+			for i in range(N):
+				row = list(map(int,input().split()))[:N]
+				for ele in row:
+					if ele not in range(1, N+1):
+						break_flag = True
+				if break_flag == False:
+					Mat.append(row)
+					if len(row) != len(set(row)):
+						r += 1
+					k = k + row[i]
+				else:
+					break
+		transpose = np.array(Mat).transpose()
+		for j in range(N):
+			if len(transpose[j]) != len(set(transpose[j])):
+				c += 1
+		K.append(k)
+		R.append(r)
+		C.append(c)
+		
+for ind in range(T):
+	print(f"Case #{ind+1}: {K[ind]} {R[ind]} {C[ind]}")
